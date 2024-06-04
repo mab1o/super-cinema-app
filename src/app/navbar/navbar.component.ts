28b1943218982229f8b7b5c8812091cbd5177c14
Margaux Blondel <margaux.blondel@free.fr>
2024-06-04 15:10:12 +0200

    feat: create nav routes for home and movies

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Input } from '@angular/core';
-import { CommonModule, TitleCasePipe } from '@angular/common';
+import { CommonModule } from '@angular/common';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
