86f3386714d876d4f1e6266353ac70920f52ecd5
Margaux Blondel <margaux.blondel@free.fr>
2024-06-04 16:18:25 +0200

    feat: add movies table to movies page

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {AsyncPipe, DatePipe, TitleCasePipe} from "@angular/common";
+import {MoviesService} from "../services/movies.service";
+import {Observable} from "rxjs";
+import {Movie} from "../models/movie";
 
 @Component({
   selector: 'app-movies',
   standalone: true,
-  imports: [],
+  imports: [
+    AsyncPipe,
+    DatePipe,
+    TitleCasePipe
+  ],
   templateUrl: './movies.component.html',
   styleUrl: './movies.component.scss'
 })
 export class MoviesComponent {
-
+  private readonly moviesService = inject(MoviesService)
+  movies$: Observable<Movie[]> = this.moviesService.getMovies()
 }
