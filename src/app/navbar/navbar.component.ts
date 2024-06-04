e3a6fd7169ac568e23b041bd5244ddca47f75baf
Margaux Blondel <margaux.blondel@free.fr>
2024-06-04 12:01:37 +0200

    feat: add title to navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { Input } from '@angular/core';
+import { CommonModule, TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
+   @Input({ required: true }) title! : string
 }
